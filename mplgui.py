e553270a7e77844126b3aea0f626756de84b555b
Danielle Lieber <lieberkd@gmail.com>
2016-06-07 21:49:49 -0400

    Added incomplete line movement with slider

--- mplgui.py
+++ mplgui.py
@@ -18,6 +18,8 @@ class PlotDisplay(QtGui.QDialog):
         # a figure instance to plot on
         self.figure = plt.figure()
 
+        self.ax = self.figure.add_subplot(111)
+
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
@@ -64,8 +66,25 @@ class PlotDisplay(QtGui.QDialog):
         curr_sld = self.sld.sliderPosition()
         self.sld.valueChanged.connect(self.sld_change)
 
+        # add line
+        self.vert_line = self.ax.axvline(x = float(self.wavenums[curr_sld]))
+
         # text edit to display wavenumber
-        self.textedit = QtGui.QTextEdit()
+        self.textedit = QtGui.QLabel()
+        self.textedit.setStyleSheet('background-color: white')
+
+        # horizontal layout to hold slider and text edit box
+        hslider_text = QtGui.QHBoxLayout()
+        hslider_text.addWidget(self.sld, 2)
+        hslider_text.addWidget(self.textedit, 1)
+
+        # group box to group elements together
+        gbox = QtGui.QGroupBox("Peak Selection")
+        gbox.setLayout(hslider_text)
+        qpalette = QtGui.QPalette()
+        qpalette.setColor(QtGui.QPalette.Dark, QtCore.Qt.white)
+        gbox.setPalette(qpalette)
+        #gbox.setStyleSheet("QGroupBox {border-radius: 9px; border:1px solid rgb(0, 0, 0); margin-top: 0.5em}")
 
         # set the layout
         layout = QtGui.QVBoxLayout()
@@ -73,33 +92,27 @@ class PlotDisplay(QtGui.QDialog):
         layout.addWidget(self.canvas)
         layout.addWidget(self.button)
         layout.addWidget(splitter)
-        layout.addWidget(self.sld)
-        layout.addWidget(self.textedit)
-        #layout.addWidget(self.drop_down_x)
-        #layout.addWidget(self.drop_down_y)
+        layout.addWidget(gbox)
         self.setLayout(layout)
 
     def plot(self):
         """"Create matplotlib plot for specific X and Y contained in my_collec"""
 
         # create an axis
-        ax = self.figure.add_subplot(111)
 
         # discards the old graph
-        ax.hold(False)
+        self.ax.hold(False)
 
         # get data from combo box
         curr_x = self.drop_down_x.itemText(self.drop_down_x.currentIndex())
         curr_y = self.drop_down_y.itemText(self.drop_down_y.currentIndex())
 
+        # plot data
         spectra_list = self.my_collec.spectra
         print(spectra_list)
         for i, spectrum in enumerate(spectra_list):
             if (spectrum.x == float(curr_x)) and (spectrum.y == float(curr_y)):
-                ax.scatter(*zip(*self.my_collec.spectra[i].info_flipped))
-
-        # plot data
-        #ax.scatter(*zip(*self.my_collec.spectra[0].info_flipped))
+                self.ax.scatter(*zip(*self.my_collec.spectra[i].info_flipped))
 
         # refresh canvas
         self.canvas.draw()
@@ -127,6 +140,10 @@ class PlotDisplay(QtGui.QDialog):
 
         self.textedit.setText(str(curr_wavenum))
 
+        self.ax.lines.remove(self.vert_line)
+        self.vert_line = self.ax.axvline(x = curr_wavenum)
+
+        self.canvas.blit()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
